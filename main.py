2939b0f00ecb1b1299ebdbd72eab63be6db03461
asenetcky <me@asenetcky.dev>
2025-06-06 07:44:44 -0400

    feat: add main,py

---
+++ main.py
@@ -0,0 +1,11 @@
+import tensorflow_data_validation as tfdv
+import polars as pl
+
+
+stats = pl.read_csv("data/spam.csv", encoding = "latin-1").to_pandas()
+stats = tfdv.generate_statistics_from_dataframe(stats)
+
+
+print(
+    stats.datasets[0].features[0].string_stats.rank_histogram
+)
